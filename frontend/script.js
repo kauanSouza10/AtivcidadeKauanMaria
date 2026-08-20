f06d34878c22ecccd89dbb587f557737fa47471a
kauanSouza10 <kauan.souza@eaportal.org>
2026-08-20 08:03:20 -0300

    Atualizado

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+async function buscarFilmes() {
+    // através do acesso a rota GET, trazer os filmes e mostrar na tela
+    const resposta = await fetch("http://localhost:3333/all-filmes");
+    const filmes = await resposta.json();
+    const sectionFilmes = document.querySelector(".filmes");
+
+    filmes.forEach((filme) => {
+        sectionFilmes.innerHTML += `
+            <div>
+                <h2>${filme.title}</h2>
+                <p><strong>Gênero:</strong> ${filme.genre}</p>
+                <p><strong>Duração:</strong> ${filme.duration} minutos</p>
+                <p><strong>Classificação indicativa:</strong> ${filme.age_rating}</p>
+            </div>
+        `;
+    });
+}
+
+buscarFilmes();
